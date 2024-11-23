2eca2ce28b8c00c9d386250e5a2f55d8330cf3cc
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-23 12:13:40 +0200

    Product
    add product endpoint

--- src/main/java/com/storemanagement/controller/ProductController.java
+++ src/main/java/com/storemanagement/controller/ProductController.java
@@ -1,7 +1,25 @@
 package com.storemanagement.controller;
 
+import com.storemanagement.model.Product;
 import com.storemanagement.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct (@RequestBody Product product) {
+        return ResponseEntity.ok(productService.addProduct(product));
+    }
 
 }
