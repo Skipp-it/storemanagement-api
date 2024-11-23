53c46a202333ac12679ef892ff37fe9d989f6d4f
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-23 12:26:08 +0200

    Find Product
    get Product by id service method

--- src/main/java/com/storemanagement/service/ProductService.java
+++ src/main/java/com/storemanagement/service/ProductService.java
@@ -4,6 +4,8 @@ import com.storemanagement.model.Product;
 import com.storemanagement.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
@@ -14,5 +16,9 @@ public class ProductService {
 
     public Product addProduct (Product product) {
         return productRepository.save(product);
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
     }
 }
