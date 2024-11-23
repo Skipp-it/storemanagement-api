803701d9f05e86f41c87a5124aa5f39cb6ba3e5c
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-23 11:24:32 +0200

    Initial commit

---
+++ src/main/java/com/storemanagement/StoremanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.storemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremanagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storemanagement/StoremanagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremanagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
