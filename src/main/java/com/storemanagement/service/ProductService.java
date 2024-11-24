14d5687c44643606efdfc4901703c2bd94971586
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-24 23:40:52 +0200

    update product service

--- src/main/java/com/storemanagement/service/ProductService.java
+++ src/main/java/com/storemanagement/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.storemanagement.service;
 
 import com.storemanagement.model.Product;
+import com.storemanagement.model.UpdateProductDto;
 import com.storemanagement.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,14 @@ public class ProductService {
 
     public Optional<Product> getProductById(Long id) {
         return productRepository.findById(id);
+    }
+
+    public Product updateProduct(Long id, UpdateProductDto updateProductDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+        product.setName(updateProductDto.name());
+        product.setDescription(updateProductDto.description());
+        product.setPrice(updateProductDto.price());
+        return productRepository.save(product);
     }
 }
