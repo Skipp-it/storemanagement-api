cbef00ae843f5ea3b256d4d19847b2ca2b637a5b
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-25 17:46:32 +0200

    UserService
    loadUserByUsername implemented to load user by username

--- src/main/java/com/storemanagement/service/UserService.java
+++ src/main/java/com/storemanagement/service/UserService.java
@@ -1,10 +1,17 @@
 package com.storemanagement.service;
 
+import com.storemanagement.model.Role;
+import com.storemanagement.model.User;
 import com.storemanagement.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,6 +24,17 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                getAuthorities(user.getRole())
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Role role) {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 }
