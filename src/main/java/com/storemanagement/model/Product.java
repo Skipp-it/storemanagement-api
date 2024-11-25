ea7560ed32a068442c0e42f73a34b491f6b909cc
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-25 14:16:11 +0200

    delete product endpoint isDeleted new field added to support soft delete

--- src/main/java/com/storemanagement/model/Product.java
+++ src/main/java/com/storemanagement/model/Product.java
@@ -1,9 +1,6 @@
 package com.storemanagement.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,4 +16,6 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+    @Column(nullable = false)
+    private boolean isDeleted=false;
 }
