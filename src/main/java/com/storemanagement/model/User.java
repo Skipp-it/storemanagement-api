1e774283d871f4868c3d2c6bf8c14a7a8cbe9f78
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-25 17:11:52 +0200

    User class implemented

--- src/main/java/com/storemanagement/model/User.java
+++ src/main/java/com/storemanagement/model/User.java
@@ -1,5 +1,25 @@
 package com.storemanagement.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
