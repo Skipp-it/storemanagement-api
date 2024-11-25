bffd9fa723e1d7293a8dfe4f257ffa1e2ed3e62d
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-25 12:37:24 +0200

    update product controller

--- src/main/java/com/storemanagement/controller/ProductController.java
+++ src/main/java/com/storemanagement/controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.storemanagement.controller;
 
 import com.storemanagement.model.Product;
+import com.storemanagement.model.UpdateProductDto;
 import com.storemanagement.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,5 +27,12 @@ public class ProductController {
     @Operation(summary = "Get product by id")
     public ResponseEntity<Product> getProduct(@PathVariable Long id) {
         return productService.getProductById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update product by id")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody UpdateProductDto updateProductDto) {
+        return productService.getProductById(id).map(product -> ResponseEntity.ok(productService.updateProduct(id, updateProductDto)))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
