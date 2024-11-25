cdd2422b0ee2ec70fc5bd4f3aa76877e5bb38693
[skipp-it] <[mihairaducroitoru@gmail.com]>
2024-11-25 17:39:41 +0200

    UserService created

---
+++ src/main/java/com/storemanagement/service/UserService.java
@@ -0,0 +1,22 @@
+package com.storemanagement.service;
+
+import com.storemanagement.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
